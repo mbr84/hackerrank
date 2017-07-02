cac7cfdf2a589bd0f4bf8e3724c50e6d004e0184
mbr84 <matthew.bramfeld@gmail.com>
2017-07-01 22:02:03 -0700

    reverse-shuffle-merge

---
+++ reverseShuffleMerge.js
@@ -0,0 +1,55 @@
+const getLetterRanks = string =>
+  string.split("")
+    .filter((v, i, a) => a.indexOf(v) === i)
+    .sort()
+const getLetterCounts = (string) => {
+  return string.split("").reduce((acc, curr) => {
+    acc[curr] ? acc[curr] += .5 : acc[curr] = .5
+    return acc
+  }, {})
+}
+const minimalString = (string) => {
+  const letterCounts = getLetterCounts(string)
+  const letterRanks = getLetterRanks(string)
+  const seen = {}
+  let result = ""
+  let bestSeen = { char: "|", idx: string.length + 1 } // "z" < "|" => true
+  const funcs = [
+      char => seen[char].used++,
+      char => result += char,
+      () => bestSeen.char = "|"
+    ]
+  const updateVars = char => funcs.forEach(fn => fn(char))
+
+  for (let i = string.length - 1; i >= 0 && result.length < string.length / 2; i--) {
+    const char = string[i]
+    seen[char] ? seen[char].seen++ : seen[char] = { seen: 1, used: 0 }
+
+    if (char === letterRanks[0]) {
+      updateVars(char)
+
+      while (seen[letterRanks[0]] && seen[letterRanks[0]].used === letterCounts[letterRanks[0]]) {
+        letterRanks.shift()
+      }
+
+    } else if (seen[char].seen > letterCounts[char] + seen[char].used) {
+
+      if (char < bestSeen.char) {
+        updateVars(char)
+
+      } else {
+        while (i < bestSeen.idx) {
+          seen[string[i]].seen--
+          i++
+        }
+        updateVars(string[i])
+      }
+    } else {
+      if (char < bestSeen.char && seen[char].used < letterCounts[char]) {
+        bestSeen.char = char
+        bestSeen.idx = i
+      }
+    }
+  }
+  return result
+}
